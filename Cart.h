45072a12776734be3a0dde076eb19cc0347bb402
DeeB313 <116029548+DeeB313@users.noreply.github.com>
2022-11-29 01:35:29 -0600

    Update Cart.h

--- Cart.h
+++ Cart.h
@@ -19,19 +19,24 @@ public:
 
     Cart();
 
-    void login(User *user); //logs in to a user
+    void login(User* user); //logs in to a user
 
     void logout(); //logs out of current user
 
     void checkout(Cart& cart); //checks out, item gets annihilated, money gets stolen
 
-    void addItem(); //adds item to cart
+    void addItem(Inventory inventory, string& item, int quantity); //adds item to cart
 
-    void removeItem(); //removes item from cart
+    void removeItem(Inventory inventory, Node* item, int quantity); //removes item from cart
 
-    void displayCart(Cart *cart); //displays cart contents
+    void displayCart(); //displays cart contents
 
     double Total(Cart& cart); //displays cart total, this will be called by displayCart
+
+    void createCart();
+
+    inline bool exist(const std::string& name);
+
 
 };
 
